9509b3fe4bd69fbe8f099415641af20bfc3e64d7
Federico Ahumada <ahufede@gmail.com>
2022-10-20 23:54:19 -0300

    first commit consultas app

---
+++ TPI LCII PARTE 3/ConsultasAutomotriz-20-10-2022/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ConsultasAutomotriz
+{
+    partial class frmConsultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultas));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboIntegrante = new System.Windows.Forms.ComboBox();
+            this.rbC1 = new System.Windows.Forms.RadioButton();
+            this.rbC2 = new System.Windows.Forms.RadioButton();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvResultados);
+            this.groupBox2.Location = new System.Drawing.Point(12, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 215);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados:";
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.AllowUserToAddRows = false;
+            this.dgvResultados.AllowUserToDeleteRows = false;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(21, 29);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.Size = new System.Drawing.Size(437, 165);
+            this.dgvResultados.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbC2);
+            this.groupBox1.Controls.Add(this.rbC1);
+            this.groupBox1.Controls.Add(this.btnConsultar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboIntegrante);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 90);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros:";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(329, 24);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(111, 23);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Integrante:";
+            // 
+            // cboIntegrante
+            // 
+            this.cboIntegrante.FormattingEnabled = true;
+            this.cboIntegrante.Items.AddRange(new object[] {
+            "Conrado-Vitelli",
+            "Diego-Nicolas-Guizzo",
+            "Federico-Ahumada",
+            "Matias-Nicolas-Villarruel"});
+            this.cboIntegrante.Location = new System.Drawing.Point(83, 24);
+            this.cboIntegrante.Name = "cboIntegrante";
+            this.cboIntegrante.Size = new System.Drawing.Size(215, 21);
+            this.cboIntegrante.TabIndex = 0;
+            // 
+            // rbC1
+            // 
+            this.rbC1.AutoSize = true;
+            this.rbC1.Location = new System.Drawing.Point(94, 59);
+            this.rbC1.Name = "rbC1";
+            this.rbC1.Size = new System.Drawing.Size(75, 17);
+            this.rbC1.TabIndex = 5;
+            this.rbC1.TabStop = true;
+            this.rbC1.Text = "Consulta 1";
+            this.rbC1.UseVisualStyleBackColor = true;
+            // 
+            // rbC2
+            // 
+            this.rbC2.AutoSize = true;
+            this.rbC2.Location = new System.Drawing.Point(201, 59);
+            this.rbC2.Name = "rbC2";
+            this.rbC2.Size = new System.Drawing.Size(75, 17);
+            this.rbC2.TabIndex = 6;
+            this.rbC2.TabStop = true;
+            this.rbC2.Text = "Consulta 2";
+            this.rbC2.UseVisualStyleBackColor = true;
+            // 
+            // frmConsultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 335);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(518, 374);
+            this.MinimumSize = new System.Drawing.Size(518, 374);
+            this.Name = "frmConsultas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CONSULTAS AUTOMOTRIZ";
+            this.Load += new System.EventHandler(this.frmConsultas_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboIntegrante;
+        private System.Windows.Forms.RadioButton rbC2;
+        private System.Windows.Forms.RadioButton rbC1;
+    }
+}
+
---
+++ TPI LCII PARTE 3/ConsultasAutomotriz-20-10-2022/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultasAutomotriz
+{
+    internal class Helper
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-R46G6B5\\SQLEXPRESS;Initial Catalog=AUTOMOTRIZ;Integrated Security=True");
+        private SqlCommand comando = new SqlCommand();
+
+        public DataTable Consultar(string NombreSP)
+        {
+            DataTable tabla = new DataTable();
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(NombreSP, conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            tabla.Load(cmd.ExecuteReader());
+            conexion.Close();
+            return tabla;
+        }
+    }
+}
